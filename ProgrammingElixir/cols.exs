1615cd3632801b840d0cacb74498d0ca378911ad
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2020-04-24 11:40:42 -0700

    changed h and t in filter and replace_at to head and tail for consistency

--- ProgrammingElixir/cols.exs
+++ ProgrammingElixir/cols.exs
@@ -16,16 +16,16 @@ defmodule Cols do
 
     def replace_at(lst, index, val), do: _replace_at(lst, index, val, [], 0)
     def _replace_at([], _index, _val, newlist, curr), do: newlist
-    def _replace_at([h | t], index, val, newlist, index), do: newlist ++ [val] ++ t
-    def _replace_at([h|t], index, val, newlist, curr), do: _replace_at(t, index, val, newlist ++ [h], curr+1)
+    def _replace_at([_head | tail], index, val, newlist, index), do: newlist ++ [val] ++ tail
+    def _replace_at([head | tail], index, val, newlist, curr), do: _replace_at(tail, index, val, newlist ++ [head], curr+1)
 
     def filter(lst, fun), do: _filter(lst, fun, [])
     def _filter([], _fun, newlist), do: newlist
-    def _filter([h | t], fun, newlist) do
-      if fun.(h) do
-       _filter(t, fun, newlist ++ [h])
+    def _filter([head | tail], fun, newlist) do
+      if fun.(head) do
+       _filter(tail, fun, newlist ++ [head])
       else
-        _filter(t, fun, newlist)
+        _filter(tail, fun, newlist)
       end
     end
 end
